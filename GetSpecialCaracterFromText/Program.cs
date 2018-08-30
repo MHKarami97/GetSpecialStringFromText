496b8ab3e26f964a1297d962ca2e1d2af9da5c75
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2018-08-30 18:25:45 +0430

    program start and finish !!!

--- GetSpecialCaracterFromText/Program.cs
+++ GetSpecialCaracterFromText/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace GetSpecialCaracterFromText
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var list = new List<string>();
+
+            var logFile = File.ReadAllLines(@"C:\Users\M-H-KARAMI\Downloads\1.xaml");
+
+            foreach (var str in logFile)
+            {
+                var mcol = Regex.Matches(str, @">\b(.+?)\b<");
+
+                foreach (Match m in mcol)
+                {
+                    list.Add(m.ToString().Replace("<", "").Replace(">", ""));
+                }
+            }
+
+            File.WriteAllLines("SavedLists.txt", list);
+
+            Console.WriteLine("Finish. Please enter any key");
+
+            Console.ReadKey();
         }
     }
 }
